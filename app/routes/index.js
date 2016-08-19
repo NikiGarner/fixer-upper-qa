6a79b2960675db798125a2c82ee896257a8fd335
NikiGarner <nbarfield5@gmail.com>
2016-08-19 13:45:37 -0700

    refactor code for dynamic routing

--- app/routes/index.js
+++ app/routes/index.js
@@ -4,21 +4,11 @@ export default Ember.Route.extend({
   model() {
     return this.store.findAll('question');
   },
-
-  actions: {
-    save3(params) {
-      var newQuestion = this.store.createRecord('question', params);
-      newQuestion.save();
-      this.transitionTo('index');
-    },
-    update(question, params) {
-      Object.keys(params).forEach(function(key){
-        if(params[key]!==undefined) {
-          question.set(key,params[key]);
-        }
-      });
-      question.save();
-      this.transitionTo('index');
-    },
+   actions: {
+      save3(params) {
+        var newQuestion = this.store.createRecord('question', params);
+        newQuestion.save();
+        this.transitionTo('index');
+      }
   }
 });
